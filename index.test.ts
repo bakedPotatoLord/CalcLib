7c71ec204057fc40ec69bcfa2e2a0615fadf5566
bakedpotatolord <918780@dpsk12.net>
2022-11-07 13:26:58 -0700

    create test suite

--- index.test.ts
+++ index.test.ts
@@ -1,28 +1,17 @@
+import {integrate,derivitiveAtX, areaAroundAxis} from "./index"
 
-/**
- * @author BakedPotatoLord
-*/
+test("integration",()=>{
+  expect(integrate((x)=> 1,0,1,0.001)).toBeCloseTo(1,0.001)
+  expect(4*integrate((x)=> Math.sqrt(1-x**2),0,1,0.0000001)).toBeCloseTo(Math.PI,0.001)
+  expect(integrate((x)=> 2*Math.cos(x),0,Math.PI/2,0.00001)).toBeCloseTo(2,0.001)
+})
 
+test("derivitives at x",()=>{
+  expect(derivitiveAtX((x)=>20*x,0,0.00001)).toBeCloseTo(20,0.001)
+  expect(derivitiveAtX((x)=>1/x,0,-0.0001)).toBeCloseTo(Infinity)
+})
 
-import {integrate,derivitiveAtX, areaAroundAxis} from "./index.js"
-
-
-console.log('test integration function')
-
-console.log(
-
-  integrate((x)=> 1,0,1,0.001), //returns 1
-
-  4*integrate((x)=> Math.sqrt(1-x**2),0,1,0.0000001), //returns ~PI
-  
-  integrate((x)=> 2*Math.cos(x),0,Math.PI/2,0.00001), //returns  2
-  
-  derivitiveAtX((x)=>20*x,0,0.00001), //returns 20
-  
-  derivitiveAtX((x)=>1/x,0,-0.0001), //returns INFINITY
-
-  areaAroundAxis((x)=>2,'x',0,1,0.0001), // returns 4*PI ~= 12.566
-
-  areaAroundAxis((x)=>x**2,'y',0,1,0.0001), // returns PI/2 ~= 1.57
-)
-
+test("areas around axis",()=>{
+  expect(areaAroundAxis((x)=>2,'x',0,1,0.0001)).toBeCloseTo(4*Math.PI,0.001)
+  expect(areaAroundAxis((x)=>x**2,'y',0,1,0.0001)).toBeCloseTo(Math.PI/2,0.001)
+})
