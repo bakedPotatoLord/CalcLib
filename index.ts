bc6b810e7de04d23d5c1213ec0c8fd91e56532c0
bakedpotatolord <918780@dpsk12.net>
2022-11-08 10:16:44 -0700

    add integrate2d function

--- index.ts
+++ index.ts
@@ -2,19 +2,13 @@
  * @author BakedPotatoLord  
 */
 
-/**
-    @description a function in terms of x such that f(x)=y
-*/
+/**@description a function in terms of x such that f(x)=y OR, an y value*/
 type derivableFunction ={
-    (x:number):number
+    (x:number):number | number
 }
-
-/**
-    @description a function in terms of x and y such that f(x,y)=z
-*/
-
+/**@description a function in terms of x and y such that f(x,y)=z OR, an z value*/
 type derivableFunction2D ={
-    (x:number,y:number):number
+    (x:number,y:number):number | number
 }
 
 export const Tau = 2*Math.PI
@@ -46,6 +40,24 @@ export function integrate(f:derivableFunction,start:number,stop:number,accuracy:
 }
 
 /** 
+    @description returns the area of a shape bounded by the curve produced by the function, bound inputs, and the xy axis
+    @param  f function to integrate
+    @param  start where to start the integration
+    @param  stop where to stop the integration
+    @param  accuracy a number between 0.00000000001 and 1 (smaller is more accurate)
+    */
+    export function integrate2D(f:derivableFunction2D,xLower:number,xUpper:number,yLower:number,yUpper:number,accuracy:number){
+        let temp = 0;
+        for(let i = xLower;i<(xUpper-accuracy);i+=(xUpper-xLower)*accuracy){
+            for(let j = yLower;i<(yUpper-accuracy);i+=(yUpper-yLower)*accuracy){
+            //for all xy pairs,
+            temp+= ((f(i,j)+f(i+accuracy,j)+f(i,j+accuracy)+f(i+accuracy,j+accuracy))/4)*((xUpper-xLower)*(yUpper-yLower)*accuracy);
+            }
+        }
+        return temp;
+    }
+
+/** 
     @description returns the slope of the tangent line at a point on a function
         (positive accuracy approaches from right. negative approaches from left)
     @param  f function to derive
@@ -72,7 +84,7 @@ export function derivitiveAtX(f:derivableFunction,point:number,accuracy: number)
     @param  start where to start the integration
     @param  stop where to stop the integration
     @param  accuracy a number between 0.00000000001 and 1 (smaller is more accurate)
-    */
+*/
 
 export function areaAroundAxis(f:derivableFunction, axis:'x'|'y',start:number,stop:number,accuracy:number){
     if(axis == 'x'){
