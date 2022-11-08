57cafe442ce3222e77e326cf6266d3bc5de269f7
bakedpotatolord <918780@dpsk12.net>
2022-11-08 10:57:38 -0700

    test numbers as function inputs

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,7 @@
 import {integrate,derivitiveAtX, areaAroundAxis, integrate2D} from "./index"
 
 test("integration",()=>{
-  expect(integrate((x)=> 1,0,1,0.001)).toBeCloseTo(1,0.001)
+  expect(integrate(1,0,1,0.001)).toBeCloseTo(1,0.001)
   expect(4*integrate((x)=> Math.sqrt(1-x**2),0,1,0.0000001)).toBeCloseTo(Math.PI,0.001)
   expect(integrate((x)=> 2*Math.cos(x),0,Math.PI/2,0.00001)).toBeCloseTo(2,0.001)
 })
@@ -12,12 +12,12 @@ test("derivitives at x",()=>{
 })
 
 test("areas around axis",()=>{
-  expect(areaAroundAxis((x)=>2,'x',0,1,0.0001)).toBeCloseTo(4*Math.PI,0.001)
+  expect(areaAroundAxis(2,'x',0,1,0.0001)).toBeCloseTo(4*Math.PI,0.001)
   expect(areaAroundAxis((x)=>x**2,'y',0,1,0.0001)).toBeCloseTo(Math.PI/2,0.001)
 })
 
 test("2dintegral",()=>{
-  expect(integrate2D((x,y)=>1,0,1,0,1,0.001)).toBeCloseTo(1,0.0001)
+  expect(integrate2D(1,0,1,0,1,0.001)).toBeCloseTo(1,0.0001)
   expect(integrate2D((x,y)=>Math.cos(x)*Math.cos(y),0,1,0,1,0.00001)).toBeCloseTo(0.708,0.0001)
 })
 
